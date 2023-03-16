c1cbc710c736bd2713820371e58f25e89b6ae561
vinisi12363 <viniciuspv.si@gmail.com>
2023-03-16 20:41:00 -0300

    adicionando o new

--- src/Pages/HabitsFeed.jsx
+++ src/Pages/HabitsFeed.jsx
@@ -1,17 +1,77 @@
+
 import styled from "styled-components";
+import { useState } from "react";
+const selectedColor = "#CFCFCF" 
+const unSelectedColor = "#FFFFFF"
 
 export default function HabitsFeed() {
-    
-    function addNewHabit(){
-        alert("plus clicked!")
-        
-    
-        
+   
+    const [weekDaysInfo, setWeekDaysInfo] = useState (
+        [
+            {
+                id: "btn1",
+                name: "D",
+                selected: false
 
-        
-   
+            },
+            {
+                id: "btn2",
+                name: "S",
+                selected: false
+
+            },
+            {
+                id: "btn3",
+                name: "T",
+                selected: false
+
+            },
+            {
+                id: "btn4",
+                name: "Q",
+                selected: false
+
+            },
+            {
+                id: "btn5",
+                name: "Q",
+                selected: false
+
+            },
+            {
+                id: "btn6",
+                name: "S",
+                selected: false
+
+            },
+            {
+                id: "btn7",
+                name: "S",
+                selected: false
+
+            },
+
+
+
+        ]
+    )
+       
+    function addNewHabit() {
+        alert("plus clicked!")
+
     }
-   
+
+    function selectDay(id) {
+        
+        const newWeekDaysInfo= weekDaysInfo.map((w => {
+            if (id === w.id) {
+                return {...w, selected: !w.selected}
+            }
+            return w
+        }))
+       setWeekDaysInfo (newWeekDaysInfo);
+    }
+
     return (
         <HabitsContainer>
             <NewHabitContainer>
@@ -25,30 +85,39 @@ export default function HabitsFeed() {
 
             </NewHabitContainer>
 
+            <NewHabit>
+
+                
+                    <input key ="newHabitInput" type="text" ></input>
+               
+                    <WeekDays>
+                        {
+                            weekDaysInfo.map((day => {
+
+                                return <DayButton
+                                    id={day.id}
+                                    color={day.selected}                                    
+                                    onClick={() => selectDay(day.id)}>
+                                    {day.name}
+                                </DayButton>
+
+                            }))
+                        }
+
+                    </WeekDays>
+
+                    <StyledP>Cancelar</StyledP>
+                    <BtnSalvar >salvar</BtnSalvar>
+                    
+
+            </NewHabit>
+
             <DefaultContainer>
                 <p>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</p>
             </DefaultContainer>
 
 
-            <NewHabit>
-                    
-                    <form>
-                        <input type="text" ></input>
-                        <WeekDays> 
-                            <DayButton>D</DayButton>
-                            <DayButton>S</DayButton>
-                            <DayButton>T</DayButton>
-                            <DayButton>Q</DayButton>
-                            <DayButton>Q</DayButton>
-                            <DayButton>S</DayButton>
-                            <DayButton>S</DayButton>
-                         </WeekDays>
-                         
-                         <p>Cancelar</p>
-                        <BtnSalvar >salvar</BtnSalvar>
-                    </form>
-                
-            </NewHabit>
+
 
         </HabitsContainer>
 
@@ -62,14 +131,16 @@ const HabitsContainer = styled.div`
     height:100%;
     display:flex;
     flex-direction:column;
+    align-items:center;
     
  
 
 `
-const NewHabitContainer = styled.div `
+const NewHabitContainer = styled.div`
     background-color:#F2F2F2;
-    width:100%;
+    width:340px;
     height:100%;
+  
     display:flex;
     flex-direction:row;
     justify-content: space-between;
@@ -81,7 +152,7 @@ const NewHabitContainer = styled.div `
         font-weight: 400;
         font-size: 22.976px;
         line-height: 29px;
-        margin-left:17px;
+        
         color: #126BA5;
     }
 
@@ -95,7 +166,7 @@ const PlusContainer = styled.div`
     flex-direction:column;
     justify-content:center;
     align-items:center;
-    margin-right:18px;
+   
 
 `
 const DefaultContainer = styled.div`
@@ -108,7 +179,6 @@ const DefaultContainer = styled.div`
         font-weight: 400;
         font-size: 17.976px;
         line-height: 22px;
-
         color: #666666;
     }
 
@@ -118,12 +188,12 @@ const DefaultContainer = styled.div`
 `
 
 
-const NewHabit = styled.div `
+const NewHabit = styled.div`
     width:340px;
     height:180px;
-    margin-left:18px;
+    margin-left:8px;
     margin-top:8px;
-
+    border-radius:5px;
     background-color:white;
     input{
         width: 303px;
@@ -138,54 +208,56 @@ const NewHabit = styled.div `
         border-radius: 5px;
     }
 
-  form p {
-    position:relative;
-    top:22px;
-    left:10px;
-    font-family: 'Lexend Deca';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 15.976px;
-    line-height: 20px;
-    /* identical to box height */
-    text-align: center;
-    color: #52B6FF;
-
-  }
-    
 
 
 `
+const StyledP = styled.p `
+    
+        position:relative;
+        top:22px;
+        left:10px;
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 15.976px;
+        line-height: 20px;
+        text-align: center;
+        color: #52B6FF;
 
-const WeekDays = styled.div `
+    
+`
+const WeekDays = styled.div`
     width:303px;
     height:35px;
     margin-left:18px;
 `
 
-const DayButton = styled.button `
+const DayButton = styled.button`
+    
     width: 30px;
     height: 30px;
-    left: 206px;
-    top: 218px;
     margin-right:5px;
-    background: #FFFFFF;
+    
     border: 1px solid #D5D5D5;
     border-radius: 5px;
-
     font-family: 'Lexend Deca';
     font-style: normal;
     font-weight: 400;
     font-size: 19.976px;
     line-height: 25px;
     /* identical to box height */
-
-
-    color: #DBDBDB;
+    color:${props => !props.color ? selectedColor : unSelectedColor};
+    background:${props => props.color ? selectedColor : unSelectedColor};
+    opacity: ${props => props.selected ? '0.5' : '1'};
+    
+     &:hover {  
+        color:${props => !props.color && 'gray'};
+        background-color: ${props => props.color ? selectedColor : 'lightgray'};
+    } 
 
 `
 
-const BtnSalvar = styled.button `
+const BtnSalvar = styled.button`
         position:relative;
         left:240px;
         bottom:20px;
