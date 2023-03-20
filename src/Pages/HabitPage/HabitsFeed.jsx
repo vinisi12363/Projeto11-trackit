4c663fb77681cad23e14a27326c0c38aa4e1607d
vinisi12363 <viniciuspv.si@gmail.com>
2023-03-20 08:30:54 -0300

    adicionando alguns data-tests , ainda falta mexer na porcentagem da barra hoje

--- src/Pages/HabitPage/HabitsFeed.jsx
+++ src/Pages/HabitPage/HabitsFeed.jsx
@@ -73,7 +73,7 @@ export default function HabitsFeed() {
     }
     function addNewHabit() {
         setBtnPlusClicked(true)
-        setIsCanceled(false)
+        
     }
 
     useEffect(() => {
@@ -139,9 +139,9 @@ export default function HabitsFeed() {
                 <NewHabitContainer>
                     <h2>Meus Hábitos</h2>
 
-                    <PlusContainer data-test="habit-create-btn" onClick={addNewHabit}>
-                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M0.244925 8.66063V6.23279H5.55921V0.837576H8.39169V6.23279H13.7599V8.66063H8.39169V13.867H5.55921V8.66063H0.244925Z" fill="white" />
+                    <PlusContainer >
+                        <svg  data-test="habit-create-btn" onClick={addNewHabit} width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path  d="M0.244925 8.66063V6.23279H5.55921V0.837576H8.39169V6.23279H13.7599V8.66063H8.39169V13.867H5.55921V8.66063H0.244925Z" fill="white" />
                         </svg>
                     </PlusContainer>
 
@@ -152,9 +152,9 @@ export default function HabitsFeed() {
                 { !haveHabits ?  (<DefaultContainer>
                         <p>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</p>
                     </DefaultContainer>)
-                    : (<SetedCardContainer data-test="habit-container" >
+                    : (<SetedCardContainer  >
                     {
-                        habitsInServer.map((h => <SetedCard  >{
+                        habitsInServer.map((h => <SetedCard  data-test="habit-container" >{
                             <>
                                 <DeleteBinContainer  data-test="habit-delete-btn"   onClick={()=>{deleteHabit(h.id)}}>
                                     <svg width="13" height="15" viewBox="0 0 13 15" fill="none" xmlns="http://www.w3.org/2000/svg">
